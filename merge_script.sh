79986cdc8badb5d54ebac46ce322d0d2ec83f3ac
Giuseppe Rosa <giuseppe.rosa@amcom.com.br>
2019-06-04 11:34:35 -0300

    Add CI v9

--- merge_script.sh
+++ merge_script.sh
@@ -6,8 +6,7 @@ fi
 export GIT_COMMITTER_EMAIL="giusepper11@gmail.com"
 export GIT_COMMITTER_NAME="Giuseppe Rosa"
 
-git remote update
-git fetch
+git fetch --depth=1 git@github.com:prefeiturasp/SME-Indice_IDEP-API.git refs/heads/other-branch:refs/remotes/origin/other-branch
 git checkout homolog || exit
 git merge "$TRAVIS_COMMIT" || exit
 git push
