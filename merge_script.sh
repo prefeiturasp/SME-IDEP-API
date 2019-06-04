d0bb2c51382f693cd5c5a832db098c9277c80ac9
Giuseppe Rosa <giuseppe.rosa@amcom.com.br>
2019-06-04 13:12:40 -0300

    Add CI v17

--- merge_script.sh
+++ merge_script.sh
@@ -6,6 +6,7 @@ fi
 export GIT_COMMITTER_EMAIL="giusepper11@gmail.com"
 export GIT_COMMITTER_NAME="Giuseppe Rosa"
 
+git pull
 git fetch --depth=1 git@github.com:prefeiturasp/SME-Indice_IDEP-API.git refs/heads/homolog:refs/remotes/origin/homolog
 git checkout homolog || exit
 git merge "$TRAVIS_COMMIT" || exit
