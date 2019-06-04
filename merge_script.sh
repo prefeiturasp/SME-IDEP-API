adcefdec650b73eb6769e1bc07805eab74beaf5b
Giuseppe Rosa <giuseppe.rosa@amcom.com.br>
2019-06-04 10:28:00 -0300

    Adicionando CI v2

--- merge_script.sh
+++ merge_script.sh
@@ -3,8 +3,8 @@ if [ "$TRAVIS_BRANCH" != "dev" ]; then
     exit 0;
 fi
 
-export GIT_COMMITTER_EMAIL=...
-export GIT_COMMITTER_NAME=...
+export GIT_COMMITTER_EMAIL= "giusepper11@gmail.com"
+export GIT_COMMITTER_NAME="Giuseppe Rosa"
 
 git checkout homolog || exit
 git merge "$TRAVIS_COMMIT" || exit
